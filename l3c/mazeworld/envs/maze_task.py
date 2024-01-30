998907d499b36fb576a441711a1088e9a99ad5fd
WorldEditors <fanwang.px@gmail.com>
2024-01-30 14:33:31 +0800

    update

--- l3c/mazeworld/envs/maze_task.py
+++ l3c/mazeworld/envs/maze_task.py
@@ -12,9 +12,9 @@ from numpy.linalg import norm
 class MazeTaskManager(object):
 
     # Configurations that decides a specific task
-    TaskConfig = namedtuple("TaskConfig", ["start", "goal", "cell_walls", "cell_texts", 
+    TaskConfig = namedtuple("TaskConfig", ["start", "cell_landmarks", "cell_walls", "cell_texts", 
         "cell_size", "wall_height", "agent_height", "initial_life", "max_life",
-        "step_reward", "goal_reward", "food_rewards", "food_interval"])
+        "step_reward", "goal_reward", "landmarks_rewards", "landmarks_refresh_interval", "commands_sequence"])
 
     def __init__(self, texture_dir, verbose=False):
         pathes = os.path.split(os.path.abspath(__file__))
@@ -46,40 +46,28 @@ class MazeTaskManager(object):
             agent_height=1.6,
             step_reward=-0.01,
             goal_reward=None,
-            food_reward=0.50,
             initial_life=1.0,
             max_life=2.0,
-            food_density=0.010,
-            food_interval=100,
-            crowd_ratio=0.0):
+            landmarks_refresh_interval=200,
+            landmarks_avg_reward=0.30,
+            landmarks_number=5,
+            commands_sequence=200,
+            crowd_ratio=0.40,
+            verbose=False):
         # Initialize the maze ...
         assert n > 6, "Minimum required cells are 7"
         assert n % 2 != 0, "Cell Numbers can only be odd"
+        assert landmarks_number > 1, "There must be at least 1 goal, thus landmarks_number must > 1"
+        assert landmarks_number < 10, "landmarks_number = %d is too much, need to be < 10" % (landmarks_number)
         if(self.verbose):
             print("Generating an random maze of size %dx%d, with allow loops=%s, crowd ratio=%f"%(n, n, allow_loops, crowd_ratio))
         cell_walls = numpy.ones(shape=(n, n), dtype="int32")
         cell_texts = numpy.random.randint(1, self.n_texts, size=(n, n))
 
-        # Dig the initial positions
+        # Dig the initial holes
         for i in range(1, n, 2):
             for j in range(1, n, 2):
                 cell_walls[i,j] = 0
-        #Randomize a start, around the bottom-left
-        s_x = random.randint(0, (n - 1) // 2 - 1) * 2 + 1
-        s_y = random.randint(0, (n - 1) // 2 - 1) * 2 + 1
-        start = (s_x, s_y)
-
-        #Randomize a goal (not quite efficiently)
-        goal = (n - 2, n - 2)
-        minimum_goal_dist = 0.45 * n #(at least that far)
-        for e_x in range(1, n, 2):
-            for e_y in range(1, n, 2):
-                e_x = random.randint(0, (n - 1) // 2 - 1) * 2 + 1
-                e_y = random.randint(0, (n - 1) // 2 - 1) * 2 + 1
-                d_s_e = numpy.sqrt((e_x - s_x) ** 2 + (e_y - s_y) ** 2)
-                if(d_s_e > minimum_goal_dist):
-                    goal = (e_x, e_y)
-                    break
 
         #Initialize the logics for prim based maze generation
         wall_dict = dict()
@@ -95,8 +83,6 @@ class MazeTaskManager(object):
                     rev_path_dict[path_idx] = [(i,j)]
                     path_idx += 1
 
-        #Prim the wall until start and goal are connected
-        #while path_dict[start] != path_dict[goal]:
         #Prim the wall until all points are connected
         max_cell_walls = numpy.product(cell_walls[1:-1, 1:-1].shape)
         while len(rev_path_dict) > 1 or (allow_loops and numpy.sum(cell_walls[1:-1, 1:-1]) > max_cell_walls * crowd_ratio):
@@ -157,6 +143,17 @@ class MazeTaskManager(object):
                 if(cell_walls[i,j] < 1):
                     cell_texts[i,j] = 0
 
+        #Randomize a start point and n landmarks
+        landmarks_likelihood = numpy.random.rand(n, n) - cell_walls
+        idxes = numpy.argsort(landmarks_likelihood, axis=None)
+        topk_idxes = idxes[-landmarks_number-1:]
+
+        def idx_trans(idx):
+            return (idx // n, idx % n)
+
+        start = idx_trans(topk_idxes[-1])
+        landmarks = [idx_trans(i) for i in topk_idxes[:-1]]
+
         #Calculate goal reward, default is - n sqrt(n) * step_reward
         assert step_reward < 0, "step_reward must be < 0"
         if(goal_reward is None):
@@ -165,17 +162,28 @@ class MazeTaskManager(object):
             def_goal_reward = goal_reward
         assert def_goal_reward > 0, "goal reward must be > 0"
 
-        # Generate food points
-        food_rewards = numpy.clip(numpy.random.rand(n, n) * food_reward, 0.10, food_reward)
-        food_rewards *= 1.0 - cell_walls
-        exp_food = (n - 1) * (n - 1) * food_density
-        while(numpy.sum(food_rewards) > exp_food): 
-            food_rewards *= (numpy.random.rand(n, n) < 0.90).astype("float32")
-        food_interval = food_interval * (food_rewards > 1.0e-3).astype("int32")
+        commands_sequence = numpy.random.randint(0, len(landmarks), commands_sequence)
+
+        # Generate landmark rewards
+        landmarks_rewards = numpy.random.rand(landmarks_number) - 0.50 + landmarks_avg_reward
+        cell_landmarks = numpy.zeros_like(cell_walls) - 1
+        for i,idx in enumerate(landmarks):
+            cell_landmarks[tuple(idx)] = int(i)
+        cell_landmarks = cell_landmarks.astype(cell_walls.dtype)
+        if(verbose):
+            print("\n\n---------Successfully generate maze task with the following attributes-----------\n")
+            print("Maze size %s x %s" %(n, n)) 
+            print("Initialze born location: %s,%s" % start)
+            integrate_maze = cell_landmarks - cell_walls
+            print("Maze configuration (-2: walls, -1 empty, >0 landmarks and ID): \n%s" % integrate_maze)
+            print("Commands sequence: \n%s" % commands_sequence)
+            print("Landmarks Rewards: \n%s" % landmarks_rewards)
+            print("Landmarks Refresh Interval: \n%s" % landmarks_refresh_interval)
+            print("\n----------------------\n\n")
 
         return MazeTaskManager.TaskConfig(
                 start=start,
-                goal=goal,
+                cell_landmarks=cell_landmarks,
                 cell_walls=cell_walls,
                 cell_texts=cell_texts,
                 cell_size=cell_size,
@@ -185,9 +193,13 @@ class MazeTaskManager(object):
                 agent_height=agent_height,
                 initial_life=initial_life,
                 max_life=max_life,
-                food_rewards=food_rewards,
-                food_interval=food_interval
+                commands_sequence=commands_sequence,
+                landmarks_rewards=landmarks_rewards,
+                landmarks_refresh_interval=landmarks_refresh_interval
                 )
 
 MAZE_TASK_MANAGER=MazeTaskManager("img")
 MazeTaskSampler = MAZE_TASK_MANAGER.sample_task
+
+if __name__=="__main__":
+    task = MazeTaskSampler(verbose=True)
